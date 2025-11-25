7f2508a402a08f79c87c29149e79f6f817e22bbc
YPINPIN <yping611@gmail.com>
2025-11-25 23:40:40 +0800

    fix task answer

--- src/main.ts
+++ src/main.ts
@@ -71,7 +71,7 @@ export const fiddleLeafFig: PlantItem = {
 // 目標：以 type 定義函式型別並實作。
 export type CartItem = { price: number; qty: number };
 export type Coupon = { type: 'percent' | 'cash'; amount: number };
-export type CalcTotalFn = (items: CartItem[], coupon: Coupon) => number;
+export type CalcTotalFn = (items: CartItem[], coupon?: Coupon) => number;
 
 export const calcTotal: CalcTotalFn = (items, coupon) => {
   const subtotal = items.reduce((sum, it) => sum + it.price * it.qty, 0);
